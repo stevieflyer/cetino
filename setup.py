a6bb4380338524bae7d3f4e7e85ac3ff7f9d644c
stevieflyer <steveflyer7@gmail.com>
2023-08-14 17:27:47 +0800

    remove redundant files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name='cetino',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=requirements,
     author='steveflyer',
